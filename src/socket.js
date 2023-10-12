59fe7a73b63256513cf3dc7914df6decf3d706bb
Dickson-Pinheiro <dicksonpinheiro4@gmail.com>
2023-10-12 02:10:51 -0300

    feat: add transports

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,5 @@
 import { io } from 'socket.io-client';
 export const socket = io("https://chat-backend-vge7.onrender.com", {
-    autoConnect: false
+    autoConnect: false,
+    transports: ['websocket']
 });
